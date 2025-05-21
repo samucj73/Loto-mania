02fe67315d51f4133bd587b2b2649802b879e0ae
samucj73 <samu.rcj@gmail.com>
2025-05-20 23:31:51 -0300

    Update cartoes_elite.py

--- cartoes_elite.py
+++ cartoes_elite.py
@@ -14,30 +14,26 @@ def calcular_quadrantes(dezenas):
                 distrib[q] += 1
     return distrib
 
-def calcular_faixa_soma_ideal(concursos):
-    somas = [sum(concurso) for concurso in concursos]
-    media = sum(somas) / len(somas)
-    desvio = (sum((x - media) ** 2 for x in somas) / len(somas)) ** 0.5
-    faixa_min = int(media - desvio)
-    faixa_max = int(media + desvio)
-    return (faixa_min, faixa_max)
-
-def cartao_valido(cartao, frequentes, soma_ideal=(850, 1250), quadrante_alvo=range(10, 16), repetidos=set()):
-    # Garante que 'frequentes' é iterável e converte para set
+def cartao_valido(cartao, frequentes, soma_ideal=(850, 1250), quadrante_alvo=range(8, 18), repetidos=set()):
     try:
         frequentes_set = set(frequentes)
     except TypeError:
         return False
 
-    if len(set(cartao) & frequentes_set) < 25:
+    # Frequência mínima mais flexível
+    if len(set(cartao) & frequentes_set) < 15:
         return False
+    # Soma ideal
     if not soma_ideal[0] <= sum(cartao) <= soma_ideal[1]:
         return False
+    # Quadrantes mais amplos
     q = calcular_quadrantes(cartao)
     if any(qv not in quadrante_alvo for qv in q.values()):
         return False
-    if len(set(cartao) & repetidos) >= 30:
+    # Tolerância maior para repetição
+    if len(set(cartao) & repetidos) >= 35:
         return False
+
     return True
 
 def conferir_acertos(cartao, concursos):
@@ -47,18 +43,16 @@ def conferir_acertos(cartao, concursos):
         resultados.append(acertos)
     return resultados
 
-def gerar_cartoes_elite(concursos, estatisticas, n_simulacoes=1000, filtro_min=16, filtro_max=20):
+def gerar_cartoes_elite(concursos, estatisticas, n_simulacoes=1000, filtro_min=18, filtro_max=20):
     elite = []
     dezenas_frequentes = estatisticas['frequencia'][:60]  # Top 60 mais frequentes
     repetidas_recentes = set()
     for c in concursos[-5:]:
         repetidas_recentes.update(c)
 
-    soma_ideal = calcular_faixa_soma_ideal(concursos)
-
     for _ in range(n_simulacoes):
         cartao = sorted(random.sample(range(100), 50))
-        if not cartao_valido(cartao, dezenas_frequentes, soma_ideal=soma_ideal, repetidos=repetidas_recentes):
+        if not cartao_valido(cartao, dezenas_frequentes, repetidos=repetidas_recentes):
             continue
 
         acertos = conferir_acertos(cartao, concursos)
